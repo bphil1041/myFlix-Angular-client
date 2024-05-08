520a3408f711261361c846c04de7a833b4e870c2
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-05-08 08:41:02 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from '../../src/app/movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +23,12 @@ export class AppComponent {
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
+    });
+  }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
     });
   }
 }
