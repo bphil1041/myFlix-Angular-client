56bd5621fc214f29bd7893254d15570e71de4b9a
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-05-24 08:25:54 -0500

    Update movie-card.component.ts

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,18 +1,19 @@
-// src/app/movie-card/movie-card.component.ts
 import { Component, OnInit } from '@angular/core';
-import { FetchApiDataService } from '../fetch-api-data.service'
-
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { DirectorInfoComponent } from '../director-info/director-info.component';
+import { GenreInfoComponent } from '../genre-info/genre-info.component';
+import { SynopsisComponent } from '../synopsis/synopsis.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
-
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
 
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(public fetchApiData: FetchApiDataService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.getMovies();
@@ -24,5 +25,26 @@ export class MovieCardComponent {
       console.log(this.movies);
       return this.movies;
     });
+  }
+
+  openDirectorInfo(director: any): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = { director };
+    dialogConfig.disableClose = false; // Ensure the modal can be closed by clicking outside
+    this.dialog.open(DirectorInfoComponent, dialogConfig);
+  }
+
+  openGenreInfo(genre: any): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = { genre };
+    dialogConfig.disableClose = false; // Ensure the modal can be closed by clicking outside
+    this.dialog.open(GenreInfoComponent, dialogConfig);
+  }
+
+  openSynopsis(movie: any): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = { movie };  // Corrected to pass the movie object
+    dialogConfig.disableClose = false;
+    this.dialog.open(SynopsisComponent, dialogConfig);
   }
 }
