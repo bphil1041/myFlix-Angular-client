b983dc7bc492302625f84763642ca7ce480e9180
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-05-27 20:35:20 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { ProfileComponent } from './profile/profile.component'; // Ensure you have this component
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: 'login', component: UserLoginFormComponent },
+  { path: 'register', component: UserRegistrationFormComponent },
+  { path: 'profile', component: ProfileComponent }, // Add this route for the profile page
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' }, // Updated pathMatch to 'full'
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
