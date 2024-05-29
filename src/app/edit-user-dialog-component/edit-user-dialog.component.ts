de0221767111447a1c00d923bb92b2737280f9c6
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-05-28 23:08:18 -0500

    Update edit-user-dialog.component.ts

--- src/app/edit-user-dialog-component/edit-user-dialog.component.ts
+++ src/app/edit-user-dialog-component/edit-user-dialog.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edit-user-dialog',
@@ -9,30 +10,27 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./edit-user-dialog.component.css'],
 })
 export class EditUserDialogComponent implements OnInit {
-  @Input() userData = {
-    UserName: '',
-    Password: '',
-    Email: '',
-    Birthdate: new Date(),
-  };
   editForm!: FormGroup;
 
   constructor(
     private fetchApiDataService: FetchApiDataService,
     public snackBar: MatSnackBar,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<EditUserDialogComponent>
   ) { }
 
   ngOnInit(): void {
     this.editForm = this.fb.group({
-      username: [this.userData.UserName, Validators.required],
-      email: [this.userData.Email, [Validators.required, Validators.email]],
+      username: [{ value: this.data.user.Username, disabled: true }],
+      newUsername: ['', Validators.required],
+      email: [this.data.user.Email, [Validators.required, Validators.email]],
       currentPassword: ['', Validators.required],
       newPassword: ['', [Validators.minLength(6)]],
       confirmNewPassword: ['', [Validators.minLength(6)]],
     }, { validator: this.passwordMatchValidator });
 
-    console.log('Initial User Data:', this.userData);
+    console.log('Initial User Data:', this.data.user);
   }
 
   passwordMatchValidator(form: FormGroup) {
@@ -40,30 +38,28 @@ export class EditUserDialogComponent implements OnInit {
       ? null : { 'mismatch': true };
   }
 
-  updateUser(): void {
+  onSave(): void {
     if (this.editForm.valid) {
       const formValue = this.editForm.value;
       const updateData = {
-        UserName: formValue.username,
+        Username: formValue.newUsername,
         Email: formValue.email,
         Password: formValue.newPassword || formValue.currentPassword,
-        Birthdate: this.userData.Birthdate
+        Birthdate: this.data.user.Birthdate
       };
 
-      console.log('Update Data:', updateData);
+      console.log('Update Data on Save:', updateData);
 
       this.fetchApiDataService.editUser(updateData).subscribe(
         (resp: any) => {
-          console.log('API Response:', resp);
-
-          this.userData = resp;
-          localStorage.setItem('user', JSON.stringify(this.userData));
+          console.log('API Response on Save:', resp);
           this.snackBar.open('Update Success', 'Close', {
             duration: 2000,
           });
+          this.dialogRef.close(resp); // Close the dialog and pass the response back to the parent component
         },
         (error) => {
-          console.error('API Error:', error);
+          console.error('API Error on Save:', error);
           this.snackBar.open('Please try again', 'No success', {
             duration: 2000,
           });
@@ -72,17 +68,7 @@ export class EditUserDialogComponent implements OnInit {
     }
   }
 
-  onSave(): void {
-    this.updateUser();
-  }
-
   onCancel(): void {
-    this.editForm.reset({
-      username: this.userData.UserName,
-      email: this.userData.Email,
-      currentPassword: '',
-      newPassword: '',
-      confirmNewPassword: ''
-    });
+    this.dialogRef.close(); // Close the dialog without passing any data
   }
 }
